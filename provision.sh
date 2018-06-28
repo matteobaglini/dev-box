35769cd6461a9d334379fe9a467577249591d29f
Matteo Baglini <matteo.baglini@gmail.com>
2018-06-28 18:58:58 +0200

    Split basic and GUI packages. Use asdf vm to install environments

--- provision.sh
+++ provision.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
 
-echo ">>>> Install basic packeges and GUI"
-sudo apt-get update -q -y
-sudo apt-get install -q -y linux-kernel-headers build-essential \
-                            git curl wget whois unzip tree \
-                            xorg xclip x11-utils autocutsel unclutter \
-                            virtualbox-guest-x11 dkms virtualbox-guest-dkms \
-                            libglib2.0-bin gnome-terminal gdm3 vim-gnome
-
 echo ">>>> Configure system settings"
 sudo timedatectl set-timezone Europe/Rome
 sudo update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
 echo "%sudo ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
+echo ">>>> Update packages"
+sudo apt-get -q -y update
+
+echo ">>>> Upgreade packages"
+sudo apt-get -q -y upgrade
+
+echo ">>>> Install basic packeges"
+sudo apt-get -q -y install \
+    build-essential autoconf linux-kernel-headers \
+    git curl wget tree whois unzip dkms gpg
+
+echo ">>>> Install GUI packages"
+sudo apt-get install -q -y \
+    xorg xclip x11-utils autocutsel unclutter \
+    gdm3 gnome-session gnome-terminal vim-gnome
+
 echo ">>>> Create custom user"
 if ! id -u matteo &>/dev/null; then
     sudo useradd --create-home \
-                    --gid users \
-                    --groups sudo \
-                    --comment "Matteo Baglini" \
-                    --password ctM0SBzcd0pi. \
-                    --shell /bin/bash \
-                    matteo
+                 --gid users \
+                 --groups sudo \
+                 --comment "Matteo Baglini" \
+                 --password ctM0SBzcd0pi. \
+                 --shell /bin/bash \
+                 matteo
     sudo chown -R matteo:users /home/matteo
 fi
-
-echo ">>>> Install Vim 8"
-sudo -i <<HEREDOC
-    if ! vim -h | grep 8 &>/dev/null; then
-        sudo apt-get remove -q -y --purge vim vim-*
-        sudo add-apt-repository ppa:pi-rho/dev
-        sudo apt-get update -q -y
-        sudo apt-get install -q -y vim-gnome
-    fi
-HEREDOC
 
 echo ">>>> Install Google Chrome"
 sudo -iu matteo <<HEREDOC
@@ -45,73 +43,43 @@ sudo -iu matteo <<HEREDOC
     fi
 HEREDOC
 
-echo "Install dotfiles"
+echo ">>>> Install dotfiles"
 sudo -iu matteo <<HEREDOC
     if [ ! -d ~/dotfiles ]; then
-        git clone --depth 1 \
-            https://github.com/matteobaglini/dotfiles.git \
-            ~/dotfiles
+        git clone https://github.com/matteobaglini/dotfiles ~/dotfiles
+        cd ~/dotfiles
+        bash install.sh
     fi
-    cd ~/dotfiles
-    bash install.sh
 HEREDOC
 
-echo "Install the latest Node version using NVM"
+echo ">>>> Install asdf & plugins"
 sudo -iu matteo <<HEREDOC
-    if [ ! -d ~/.nvm ]; then
-        wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-    fi
-    source ~/.nvm/nvm.sh
-    if ! node -v &>/dev/null; then
-        nvm install node
-        nvm alias default node
+    if [ ! -d ~/.asdf ]; then
+        git clone https://github.com/asdf-vm/asdf ~/.asdf --branch v0.5.0
+        echo -e '\n. ~/.asdf/asdf.sh' >> ~/.bashrc
+        echo -e '\n. ~/.asdf/completions/asdf.bash' >> ~/.bashrc
+
+        ~/.asdf/bin/asdf plugin-add dotnet-core https://github.com/emersonsoares/asdf-dotnet-core
+        ~/.asdf/bin/asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs
+        bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+        ~/.asdf/bin/asdf plugin-add java https://github.com/skotchpine/asdf-java
+        ~/.asdf/bin/asdf plugin-add maven https://github.com/skotchpine/asdf-maven
+        ~/.asdf/bin/asdf plugin-add scala https://github.com/mtatheonly/asdf-scala
+        ~/.asdf/bin/asdf plugin-add sbt https://github.com/gabrielelana/asdf-sbt
     fi
 HEREDOC
 
-echo "Install Java"
-sudo -iu matteo <<HEREDOC
-    if ! javac -version &>/dev/null; then
-        sudo add-apt-repository ppa:webupd8team/java
-        sudo apt-get update -q -y 
-        echo oracle-java8-set-default shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-        sudo apt-get install -q -y oracle-java8-set-default
-    fi
-HEREDOC
-
-echo "Install Scala and SBT"
-sudo -iu matteo <<HEREDOC
-    if ! scala -version &>/dev/null; then
-        wget -q http://www.scala-lang.org/files/archive/scala-2.12.2.deb
-        sudo dpkg -i scala-2.12.2.deb
-        wget -q http://dl.bintray.com/sbt/debian/sbt-0.13.15.deb
-        sudo dpkg -i sbt-0.13.15.deb
-        sudo apt-get -q -y update
-        sudo apt-get -q -y install scala sbt
-    fi
-HEREDOC
-
-echo "Install .NET Core"
-sudo -iu matteo <<HEREDOC
-    if ! dotnet --version &>/dev/null; then
-        sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
-        sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-        sudo apt-get update -q -y
-        sudo apt-get install -q -y dotnet-dev-1.0.4
-    fi
-HEREDOC
-
-echo "Install Docker and tools"
+echo ">>>> Install Docker & tools"
 sudo -i <<HEREDOC
     if ! docker version &>/dev/null; then
         wget -qO- https://get.docker.com/ | bash
         sudo usermod -aG docker matteo
     fi
     if ! docker-compose --version &>/dev/null; then
-        curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` \
+        curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` \
                 > /usr/local/bin/docker-compose
         chmod +x /usr/local/bin/docker-compose
     fi
 HEREDOC
 
-echo ">>>> Remember to reboot the box"
 echo ">>>> That's all, rock on!"
